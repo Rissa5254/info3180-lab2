8825eaae34975ad26e3fde0c52db48954eb01a9e
Marissa O'Meally <marissaomeally21@gmail.com>
2026-02-18 22:18:23 -0500

    Added route and template

--- app/views.py
+++ app/views.py
@@ -18,6 +18,12 @@ def about():
     return render_template('about.html', name="Mary Jane")
 
 
+@app.route('/profile/')
+def profile():
+    """Render the website's profile page."""
+    return render_template('profile.html')
+
+
 ###
 # The functions below should be applicable to all Flask apps.
 ###
